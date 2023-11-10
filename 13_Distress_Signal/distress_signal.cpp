7c4e72c62c37004c28c166c1586578a69e5be799
ininieto <ininieto8@gmail.com>
2023-11-10 20:03:07 +0100

    Created file for day 13

---
+++ 13_Distress_Signal/distress_signal.cpp
@@ -0,0 +1,15 @@
+/*
+    Advent of Code 2022 - 13.12.2022
+
+    Link to the problem: https://adventofcode.com/2022/day/13
+
+    Author of the solution: Íñigo Nieto Cuadrado
+
+*/
+
+int main(){
+
+
+
+    return 0;
+}
