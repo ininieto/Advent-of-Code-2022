e8a13f3ab8225d32fbab22e38cc6ef32f82a55eb
ininieto <ininieto8@gmail.com>
2023-11-11 10:39:24 +0100

    Skeleton to read the instructions

--- 13_Distress_Signal/distress_signal.cpp
+++ 13_Distress_Signal/distress_signal.cpp
@@ -7,7 +7,48 @@
 
 */
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+std::string readInputText(std::string inputText){
+
+    // Read input data from txt file
+    std::fstream inputfile;
+    std::string inputData;
+
+    inputfile.open(inputText, std::ios::in);
+    if (inputfile.is_open()){
+        std::string tp;
+        while (getline(inputfile, tp)){
+            inputData += tp;
+            inputData += "\n";
+        }
+    }
+    return inputData;
+}
+
 int main(){
+
+    std::string example = readInputText("example.txt");
+    std::string inputData = readInputText("input.txt");
+
+    std::string line;
+    std::vector<std::string> pair;
+
+    for(char c: example){
+        if(c != '\n')
+            line.push_back(c);
+        else{
+
+            if(line == "")  // If space between two lists
+                pair.clear();
+            else{
+                pair.push_back(line);
+                line.clear();
+            }
+        }
+    }
 
 
 
