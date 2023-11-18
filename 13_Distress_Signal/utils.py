0fb5bc3e4a764aa0fe79e71c52820d21eaefef29
ininieto <ininieto8@gmail.com>
2023-11-18 11:15:22 +0100

    Implemented recursive logic

--- 13_Distress_Signal/utils.py
+++ 13_Distress_Signal/utils.py
@@ -34,12 +34,17 @@ def create_packet_pairs(packets_list):
     packet_pairs = [packets_list[i:i+2] for i in range(0, len(packets_list), 2)]
     return packet_pairs
 
+# The big boy. This function will perform all the logic of comparing the lists
+
+# TODO: As the function is now recirsive, the idea of the indexes for summing up the result is not working
+# In addition, must check when a pair is already compared. In Example 8 it works comparing the lists, but after
+# giving a result, it jumps back and start comparing 8-8 and 9-9. Must fix that too
+
 def get_result(packet_pairs):
 
     result = 0
 
-
-    for i in range(len(packet_pairs)):
+    for i in range(len(packet_pairs)):  # I perform a C-style loop because I need to keep track of the indexes
 
         pair = packet_pairs[i]
 
@@ -58,38 +63,51 @@ def get_result(packet_pairs):
         
         # Iterate the packets at the same time
         for j in range(lower_length):
+
+            # Obtain the elements of both packets with the same index
             element_first_packet = first_packet[j]
             element_second_packet = second_packet[j]
 
-            # TODO: Check the type of the elements. Must be aware of comparing lists        
+            # If both elements are int, just skip the type matching code
 
-            if type(element_first_packet) != type(element_second_packet):
-                if type(element_first_packet) == int:
-                    element_first_packet = [element_first_packet]
-                else:
-                    element_second_packet = [element_second_packet]
+            # If one element is a list and the other an int, must convert the int to a list
+            if type(element_first_packet) == list and type(element_second_packet) == int:
+                element_second_packet = [element_second_packet]
+                list_1 = [element_first_packet, element_second_packet]
+                list_list_1 = [list_1]
+                get_result(list_list_1)
 
-            
-            # TODO: It fails when comparing int and nested list. I'm only converting 
-            # once int to list. More comprobations needed
+            elif type(element_first_packet) == int and type(element_second_packet) == list:
+                element_first_packet = [element_first_packet]
+                list_1 = [element_first_packet, element_second_packet]
+                list_list_1 = [list_1]
+                get_result(list_list_1)
 
-            # This way of comparing also works with lists :)
-            if element_first_packet < element_second_packet:
-                print("Right order")
-                result += (i + 1)
-                decision_made = True
-                break
-            elif element_first_packet > element_second_packet:
-                print("Wrong order")
-                decision_made = True
-                break
+            # If they both are lists, I will call this same function recursively
+            elif type(element_first_packet) == list and type(element_second_packet) == list:
+                list_1 = [element_first_packet, element_second_packet]
+                list_list_1 = [list_1]
+                get_result(list_list_1)
+
+
+            # Compare the numbers
+            if type(element_first_packet) == int and type(element_second_packet) == int:
+                if element_first_packet < element_second_packet:
+                    print("Right order")
+                    result += (i + 1)
+                    decision_made = True
+                    break
+                elif element_first_packet > element_second_packet:
+                    print("Wrong order")
+                    decision_made = True
+                    break
         
+        # TODO: Make sure that this part of the code must stay unaltered
         # If there hasn't been a decision so far, the smaller list should be first
-
         if not decision_made:
             if len(first_packet) > len(second_packet):
                 print("Wrong order")
-            else:
+            elif len(second_packet) > len(first_packet):
                 print("Right order")
                 result += (i + 1)
 
