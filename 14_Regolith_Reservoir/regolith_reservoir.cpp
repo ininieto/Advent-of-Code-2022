4a4313aad52157a1f543e263d78965b12c1a5332
ininieto <ininieto8@gmail.com>
2023-11-21 15:56:53 +0100

    Added function to split strings by delim

--- 14_Regolith_Reservoir/regolith_reservoir.cpp
+++ 14_Regolith_Reservoir/regolith_reservoir.cpp
@@ -20,13 +20,55 @@
 #include <iostream>
 #include <vector>
 
+
+// Function to split a std::string by a specific delimitator
+std::vector<std::string> split(std::string text, std::string delim){
+
+    std::vector<std::string> splittedText;
+    size_t pos = 0;
+    std::string token;
+
+    while((pos = text.find(delim)) != std::string::npos){
+        token = text.substr(0, pos);
+        splittedText.push_back(token);
+        text.erase(0, pos + delim.length());
+    }
+
+    splittedText.push_back(text);// The last element is not added to the vector otherwise
+    return splittedText;
+}
+
 int main(){
 
-    int high = 10; // I know it for the example, in the real problem I will need to guess this
-    int wide = 10;
 
-    std::string example = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9\n";
+    std::string example = "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9";
 
+    // Split the string by lines
+    std::vector<std::string> splittedExample = split(example, "\n");
+
+    // Store the data in a vector
+
+    // std::vector<std::vector<std::pair<int, int>>> rockPositions;
+
+    for(std::string line: splittedExample){
+        
+        std::vector<std::string> instruction = split(line, " -> "); // Separate the 
+
+        std::vector<std::pair<int, int>> rocks;
+        for(std::string position: instruction){
+            // Separate the x and y coord, convert to int and store in a vector (the line looks illegible tbh)
+            rocks.push_back(std::make_pair(stoi(position.substr(0, position.find(','))), stoi(position.substr(position.find(',') + 1))));   // Store the coordinates in a vector
+        }
+
+        // It could be good to place the rocks in the grid here, but I still don't know the high/wide :( Must be done later
+    }
+
+
+
+    
+    int high = 10, wide = 10; // I know it for the example, in the real problem I will need to guess this
+    
+    
     std::vector<std::vector<char>> grid(high, std::vector<char>(wide));
 
     // Set the grid to empty
