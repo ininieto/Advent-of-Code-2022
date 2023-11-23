b0e448907dde8e67a297d3379a0df006416cda58
ininieto <ininieto8@gmail.com>
2023-11-23 17:23:07 +0100

    Addressed comments

--- 14_Regolith_Reservoir/regolith_reservoir.cpp
+++ 14_Regolith_Reservoir/regolith_reservoir.cpp
@@ -60,7 +60,7 @@ void printGrid(std::vector<std::vector<char>> grid){
     std::cout << '\n';
 }
 
-void placeRocksInGrid(std::vector<std::vector<std::pair<int, int>>> rockPositions, std::vector<std::vector<char>> &grid, int conversion){
+void placeRocksInGrid(std::vector<std::vector<std::pair<int, int>>> rockPositions, std::vector<std::vector<char>> &grid, int offset){
 
     for(auto line: rockPositions){
         for(int i = 1; i < line.size(); i++){
@@ -72,22 +72,18 @@ void placeRocksInGrid(std::vector<std::vector<std::pair<int, int>>> rockPosition
 
             if(xDiff == 0){
 
-                int x = corner.first - conversion;
+                int x = corner.first - offset;
                 int yStart = (prevCorner.second < corner.second) ? prevCorner.second : corner.second;
                 int yEnd = (prevCorner.second < corner.second) ? corner.second + 1 : prevCorner.second + 1;
                  
-                for(int y = yStart; y < yEnd; y++){
-                    grid[y][x] = '#';
-                }
+                for(int y = yStart; y < yEnd; y++) grid[y][x] = '#';
             }
             else{   // If yDiff == 0
                 int y = corner.second;
                 int xStart = (prevCorner.first < corner.first) ? prevCorner.first : corner.first;
                 int xEnd = (prevCorner.first < corner.first) ? corner.first + 1: prevCorner.first + 1;
 
-                for(int x = xStart - conversion; x < xEnd - conversion; x++){
-                    grid[corner.second][x] = '#';
-                }
+                for(int x = xStart - offset; x < xEnd - offset; x++) grid[corner.second][x] = '#';
             }
         }  
     }
@@ -99,19 +95,22 @@ void placeRocksInGrid(std::vector<std::vector<std::pair<int, int>>> rockPosition
 */
 void sandFlow(std::pair<int, int> initialPosition, std::vector<std::vector<char>> &grid){
 
-    if(grid[initialPosition.first + 1][initialPosition.second] == '.'){             // Check bottom position
+    // TODO: Must check here that we're not out of bounds
+
+    /*
+    if(outOfBounds)
+        return somethingToKnowThatWeFinish
+    */
+
+    if(grid[initialPosition.first + 1][initialPosition.second] == '.')             // Check bottom position
         return sandFlow(std::make_pair(initialPosition.first + 1, initialPosition.second), grid);
-    }
-    else if(grid[initialPosition.first + 1][initialPosition.second - 1] == '.'){    // Check bottom-left position
+    else if(grid[initialPosition.first + 1][initialPosition.second - 1] == '.')    // Check bottom-left position
         return sandFlow(std::make_pair(initialPosition.first + 1, initialPosition.second - 1), grid);
-    }
-    else if(grid[initialPosition.first + 1][initialPosition.second + 1] == '.'){    // Check bottom-right position
+    else if(grid[initialPosition.first + 1][initialPosition.second + 1] == '.')    // Check bottom-right position
         return sandFlow(std::make_pair(initialPosition.first + 1, initialPosition.second + 1), grid);
-    }
 
+    // Place the sandball if the ball cannot move more 
     grid[initialPosition.first][initialPosition.second] = 'o';
-    printGrid(grid);
-
 }
 
 int main(){
@@ -124,7 +123,6 @@ int main(){
     std::vector<std::string> splittedData = split(inputData, "\n");
 
     // Store the data in a vector
-
     std::vector<std::vector<std::pair<int, int>>> rockPositions;
 
     for(std::string line: splittedExample){
@@ -157,7 +155,7 @@ int main(){
 
     int high = maxY + 1;    // Could be possible to establish margins 
     int wide = maxX - minX + 1;
-    int conversion = minX;  // This will be our reference
+    int offset = minX;  // This will be our reference
     
    // Define the grid 
     std::vector<std::vector<char>> grid(high, std::vector<char>(wide));
@@ -170,17 +168,15 @@ int main(){
     }
 
     // Set the Sand Source
-    std::pair<int, int> sourcePosition = std::make_pair(0, 500 - conversion);
+    std::pair<int, int> sourcePosition = std::make_pair(0, 500 - offset);
     grid[sourcePosition.first][sourcePosition.second] = '+';
 
     // Set the rocks in the grid
-    placeRocksInGrid(rockPositions, grid, conversion);
-
-    printGrid(grid);
-
-    int numBalls = 0;
+    placeRocksInGrid(rockPositions, grid, offset);
 
     // First attempt just with one block. Will probably need to call it inside a while loop
+    int numBalls = 0;
+    
     while(1){
         sandFlow(sourcePosition, grid);
         printGrid(grid);
